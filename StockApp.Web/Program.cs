17a6ef2e84007036ef3e04051ef3267b14597a4a
odimsom <20231065@itla.edu.do>
2025-05-06 20:25:10 -0400

    Preparacion de la percistencia de los datos

--- StockApp.Web/Program.cs
+++ StockApp.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using StokApp.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StockAppContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -23,5 +28,4 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
